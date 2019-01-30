8fe339e99873e282e98f83537cbd109660721ffc
Jaakko Malkki <jaakko.malkki@helsinki.fi>
2019-01-30 20:58:14 +0200

    Create enum describing possible transport modes

---
+++ src/main/java/xyz/malkki/gtfsroutefinder/gtfs/model/TransportMode.java
@@ -0,0 +1,65 @@
+package xyz.malkki.gtfsroutefinder.gtfs.model;
+
+public enum TransportMode {
+    RAIL, BUS, SUBWAY, TRAM, FERRY, AIRPLANE, CABLE_CAR, GONDOLA, FUNICULAR, WALK;
+
+    /**
+     * Parses {@link TransportMode} from GTFS type
+     * See list of possible types here: https://developers.google.com/transit/gtfs/reference/#routestxt and https://developers.google.com/transit/gtfs/reference/extended-route-types
+     * Note that taxi and car modes are not supported
+     * @param type GTFS transport type
+     * @returne Transport mode corresponding to the GTFS type
+     */
+    public static TransportMode getTransportModeFromGTFSType(int type) {
+        if (type >= 100 && type < 200) {
+            return TransportMode.RAIL;
+        } else if (type >= 200 && type < 300) {
+            return TransportMode.BUS;
+        } else if (type >= 300 && type < 500) {
+            if (type >= 401 && type <= 402) {
+                return TransportMode.SUBWAY;
+            }
+            
+            return TransportMode.RAIL;
+        } else if (type >= 500 && type < 700) {
+            return TransportMode.SUBWAY;
+        } else if (type >= 700 && type < 900) {
+            return TransportMode.BUS;
+        } else if (type >= 900 && type < 1000) {
+            return TransportMode.TRAM;
+        } else if (type >= 1000 && type < 1100) {
+            return TransportMode.FERRY;
+        } else if (type >= 1100 && type < 1200) {
+            return TransportMode.AIRPLANE;
+        } else if (type >= 1200 && type < 1300) {
+            return TransportMode.FERRY;
+        } else if (type >= 1300 && type < 1400) {
+            return TransportMode.GONDOLA;
+        } else if (type >= 1400 && type < 1500) {
+            return TransportMode.FUNICULAR;
+        } else if (type >= 1500 && type < 1700) {
+            throw new IllegalArgumentException("Unsupported type " + type);
+        }
+        
+        switch (type) {
+            case 0:
+                return TransportMode.TRAM;
+            case 1:
+                return TransportMode.SUBWAY;
+            case 2:
+                return TransportMode.RAIL;
+            case 3:
+                return TransportMode.BUS;
+            case 4:
+                return TransportMode.FERRY;
+            case 5:
+                return TransportMode.CABLE_CAR;
+            case 6:
+                return TransportMode.GONDOLA;
+            case 7:
+                return TransportMode.FUNICULAR;
+            default:
+                throw new IllegalArgumentException("Unknown GTFS type " + type);
+        }
+    }
+}
---
+++ src/test/java/xyz/malkki/gtfsroutefinder/gtfs/utils/model/TransportModeTest.java
@@ -0,0 +1,173 @@
+package xyz.malkki.gtfsroutefinder.gtfs.utils.model;
+
+import org.junit.Test;
+import xyz.malkki.gtfsroutefinder.gtfs.model.TransportMode;
+
+import static org.junit.Assert.*;
+
+public class TransportModeTest {
+    @Test
+    public void testGetFromGTFS1() {
+        assertEquals(TransportMode.RAIL, TransportMode.getTransportModeFromGTFSType(100));
+    }
+
+    @Test
+    public void testGetFromGTFS2() {
+        assertEquals(TransportMode.RAIL, TransportMode.getTransportModeFromGTFSType(199));
+    }
+
+    @Test
+    public void testGetFromGTFS3() {
+        assertEquals(TransportMode.BUS, TransportMode.getTransportModeFromGTFSType(200));
+    }
+
+    @Test
+    public void testGetFromGTFS4() {
+        assertEquals(TransportMode.BUS, TransportMode.getTransportModeFromGTFSType(299));
+    }
+
+    @Test
+    public void testGetFromGTFS5() {
+        assertEquals(TransportMode.RAIL, TransportMode.getTransportModeFromGTFSType(300));
+    }
+
+    @Test
+    public void testGetFromGTFS6() {
+        assertEquals(TransportMode.RAIL, TransportMode.getTransportModeFromGTFSType(499));
+    }
+
+    @Test
+    public void testGetFromGTFS7() {
+        assertEquals(TransportMode.SUBWAY, TransportMode.getTransportModeFromGTFSType(401));
+    }
+
+    @Test
+    public void testGetFromGTFS8() {
+        assertEquals(TransportMode.SUBWAY, TransportMode.getTransportModeFromGTFSType(500));
+    }
+
+    @Test
+    public void testGetFromGTFS9() {
+        assertEquals(TransportMode.SUBWAY, TransportMode.getTransportModeFromGTFSType(699));
+    }
+
+    @Test
+    public void testGetFromGTFS10() {
+        assertEquals(TransportMode.BUS, TransportMode.getTransportModeFromGTFSType(700));
+    }
+
+    @Test
+    public void testGetFromGTFS11() {
+        assertEquals(TransportMode.BUS, TransportMode.getTransportModeFromGTFSType(899));
+    }
+
+    @Test
+    public void testGetFromGTFS12() {
+        assertEquals(TransportMode.TRAM, TransportMode.getTransportModeFromGTFSType(900));
+    }
+
+    @Test
+    public void testGetFromGTFS13() {
+        assertEquals(TransportMode.TRAM, TransportMode.getTransportModeFromGTFSType(999));
+    }
+
+    @Test
+    public void testGetFromGTFS14() {
+        assertEquals(TransportMode.FERRY, TransportMode.getTransportModeFromGTFSType(1000));
+    }
+
+    @Test
+    public void testGetFromGTFS15() {
+        assertEquals(TransportMode.FERRY, TransportMode.getTransportModeFromGTFSType(1099));
+    }
+
+    @Test
+    public void testGetFromGTFS16() {
+        assertEquals(TransportMode.AIRPLANE, TransportMode.getTransportModeFromGTFSType(1100));
+    }
+
+    @Test
+    public void testGetFromGTFS17() {
+        assertEquals(TransportMode.AIRPLANE, TransportMode.getTransportModeFromGTFSType(1199));
+    }
+
+    @Test
+    public void testGetFromGTFS18() {
+        assertEquals(TransportMode.FERRY, TransportMode.getTransportModeFromGTFSType(1200));
+    }
+
+    @Test
+    public void testGetFromGTFS19() {
+        assertEquals(TransportMode.FERRY, TransportMode.getTransportModeFromGTFSType(1299));
+    }
+
+    @Test
+    public void testGetFromGTFS20() {
+        assertEquals(TransportMode.GONDOLA, TransportMode.getTransportModeFromGTFSType(1300));
+    }
+
+    @Test
+    public void testGetFromGTFS21() {
+        assertEquals(TransportMode.GONDOLA, TransportMode.getTransportModeFromGTFSType(1399));
+    }
+
+    @Test
+    public void testGetFromGTFS22() {
+        assertEquals(TransportMode.FUNICULAR, TransportMode.getTransportModeFromGTFSType(1400));
+    }
+
+    @Test
+    public void testGetFromGTFS23() {
+        assertEquals(TransportMode.FUNICULAR, TransportMode.getTransportModeFromGTFSType(1499));
+    }
+
+    @Test
+    public void testGetFromGTFS24() {
+        assertEquals(TransportMode.TRAM, TransportMode.getTransportModeFromGTFSType(0));
+    }
+
+    @Test
+    public void testGetFromGTFS25() {
+        assertEquals(TransportMode.SUBWAY, TransportMode.getTransportModeFromGTFSType(1));
+    }
+
+    @Test
+    public void testGetFromGTFS26() {
+        assertEquals(TransportMode.RAIL, TransportMode.getTransportModeFromGTFSType(2));
+    }
+
+    @Test
+    public void testGetFromGTFS27() {
+        assertEquals(TransportMode.BUS, TransportMode.getTransportModeFromGTFSType(3));
+    }
+
+    @Test
+    public void testGetFromGTFS28() {
+        assertEquals(TransportMode.FERRY, TransportMode.getTransportModeFromGTFSType(4));
+    }
+
+    @Test
+    public void testGetFromGTFS29() {
+        assertEquals(TransportMode.CABLE_CAR, TransportMode.getTransportModeFromGTFSType(5));
+    }
+
+    @Test
+    public void testGetFromGTFS30() {
+        assertEquals(TransportMode.GONDOLA, TransportMode.getTransportModeFromGTFSType(6));
+    }
+
+    @Test
+    public void testGetFromGTFS31() {
+        assertEquals(TransportMode.FUNICULAR, TransportMode.getTransportModeFromGTFSType(7));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetFromGTFSUnsupported() {
+        TransportMode.getTransportModeFromGTFSType(1600);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetFromGTFSUnknown() {
+        TransportMode.getTransportModeFromGTFSType(80);
+    }
+}
