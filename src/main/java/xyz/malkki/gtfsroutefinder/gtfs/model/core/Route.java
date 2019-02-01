f75b4923aea84ef61fbcdcd56833ad6fc1e37fcc
Jaakko Malkki <jaakko.malkki@helsinki.fi>
2019-02-01 17:42:12 +0200

    Add 'mode' field to Route

--- src/main/java/xyz/malkki/gtfsroutefinder/gtfs/model/core/Route.java
+++ src/main/java/xyz/malkki/gtfsroutefinder/gtfs/model/core/Route.java
@@ -1,6 +1,7 @@
 package xyz.malkki.gtfsroutefinder.gtfs.model.core;
 
 import xyz.malkki.gtfsroutefinder.common.model.LatLng;
+import xyz.malkki.gtfsroutefinder.gtfs.model.TransportMode;
 import xyz.malkki.gtfsroutefinder.gtfs.utils.GTFSParser;
 
 import java.io.IOException;
@@ -11,8 +12,9 @@ import java.util.Objects;
 public class Route {
     private String id;
     private String name;
+    private TransportMode mode;
 
-    public Route(String id, String name) {
+    public Route(String id, String name, TransportMode mode) {
         this.id = id;
         this.name = name;
     }
@@ -25,18 +27,23 @@ public class Route {
         return name;
     }
 
+    public TransportMode getMode() {
+        return mode;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Route route = (Route) o;
         return Objects.equals(id, route.id) &&
-                Objects.equals(name, route.name);
+                Objects.equals(name, route.name) &&
+                mode == route.mode;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name);
+        return Objects.hash(id, name, mode);
     }
 
     public static List<Route> parseFromFile(String file) throws IOException {
@@ -44,8 +51,9 @@ public class Route {
             String id = record.get("route_id");
             String shortName = record.get("route_short_name");
             String longName = record.get("route_long_name");
+            int transportMode = Integer.parseInt(record.get("route_type"));
 
-            return new Route(id, shortName == null || shortName.isEmpty() ? longName : shortName);
+            return new Route(id, shortName == null || shortName.isEmpty() ? longName : shortName, TransportMode.getTransportModeFromGTFSType(transportMode));
         });
     }
 }
--- src/test/java/xyz/malkki/gtfsroutefinder/gtfs/utils/model/core/RouteTest.java
+++ src/test/java/xyz/malkki/gtfsroutefinder/gtfs/utils/model/core/RouteTest.java
@@ -3,6 +3,7 @@ package xyz.malkki.gtfsroutefinder.gtfs.utils.model.core;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import xyz.malkki.gtfsroutefinder.gtfs.model.TransportMode;
 import xyz.malkki.gtfsroutefinder.gtfs.model.core.Calendar;
 import xyz.malkki.gtfsroutefinder.gtfs.model.core.Route;
 
@@ -15,33 +16,33 @@ public class RouteTest {
 
     @Before
     public void setup() {
-        route = new Route("test", "test_name");
+        route = new Route("test", "test_name", TransportMode.BUS);
     }
 
     @Test
     public void testHashCodeIsSame() {
-        Route other = new Route("test", "test_name");
+        Route other = new Route("test", "test_name", TransportMode.BUS);
 
         assertEquals(route.hashCode(), other.hashCode());
     }
 
     @Test
     public void testEqualsWhenSame() {
-        Route other = new Route("test", "test_name");
+        Route other = new Route("test", "test_name", TransportMode.BUS);
 
         assertTrue(route.equals(other));
     }
 
     @Test
     public void testHashCodeIsNotSame() {
-        Route other = new Route("fsdaoifnsaf", "test_name");
+        Route other = new Route("fsdaoifnsaf", "test_name", TransportMode.TRAM);
 
         assertNotEquals(route.hashCode(), other.hashCode());
     }
 
     @Test
     public void testNotEqualsWhenNotSame() {
-        Route other = new Route("fsdaoifnsaf", "gfdsgsdfg");
+        Route other = new Route("fsdaoifnsaf", "gfdsgsdfg", TransportMode.TRAM);
 
         assertFalse(route.equals(other));
     }
@@ -50,5 +51,6 @@ public class RouteTest {
     public void testGetters() {
         assertEquals("test", route.getId());
         assertEquals("test_name", route.getName());
+        assertEquals(TransportMode.BUS, route.getMode());
     }
 }
