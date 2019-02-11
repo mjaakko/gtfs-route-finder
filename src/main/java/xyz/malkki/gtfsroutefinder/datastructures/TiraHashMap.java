41f02168e150a6c1739133bfd0e04ef13002290f
Jaakko Malkki <jaakko.malkki@helsinki.fi>
2019-02-11 20:06:29 +0200

    Create custom HashMap implementation

---
+++ src/main/java/xyz/malkki/gtfsroutefinder/datastructures/TiraHashMap.java
@@ -0,0 +1,248 @@
+package xyz.malkki.gtfsroutefinder.datastructures;
+
+import java.util.*;
+
+public class TiraHashMap<K, V> extends AbstractMap<K, V> {
+    private TiraLinkedList<Entry<K,V>>[] entries = new TiraLinkedList[10];
+
+    private int itemCount = 0;
+
+    private void increaseCapacity() {
+        TiraLinkedList<Entry<K,V>>[] newArray = new TiraLinkedList[2 * (1 + entries.length)];
+        for (TiraLinkedList<Entry<K,V>> list : entries) {
+            if (list == null) {
+                continue;
+            }
+            for (Entry<K,V> item : list) {
+                int index = getIndex(item.getKey(), newArray.length);
+
+                if (newArray[index] == null) {
+                    newArray[index] = new TiraLinkedList<>();
+                }
+                newArray[index].add(item);
+            }
+        }
+
+        entries = newArray;
+    }
+
+
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        return new EntrySet();
+    }
+
+    @Override
+    public void clear() {
+        entries = new TiraLinkedList[10];
+
+        itemCount = 0;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        int index = getIndex(key, entries.length);
+
+        if (entries[index] == null || entries[index].isEmpty()) {
+            return false;
+        } else {
+            for (Entry<K, V> entry : entries[index]) {
+                if (entry.getKey().equals(key)) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        for (TiraLinkedList<Entry<K, V>> entryList : entries) {
+            if (entryList != null) {
+                for (Entry<K, V> entry : entryList) {
+                    if (entry.getValue().equals(value)) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public V remove(Object key) {
+        int index = getIndex(key, entries.length);
+        if (entries[index] == null) {
+            return null;
+        }
+
+        for (int i = 0; i < entries[index].size(); i++) {
+            Entry<K, V> e = entries[index].get(i);
+            if (e.getKey().equals(key)) {
+                entries[index].remove(i);
+                itemCount--;
+                return e.getValue();
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public V get(Object key) {
+        int index = getIndex(key, entries.length);
+        if (entries[index] == null) {
+            return null;
+        }
+
+        for (Entry<K, V> entry : entries[index]) {
+            if (entry.getKey().equals(key)) {
+                return entry.getValue();
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public V put(K key, V value) {
+        if (value == null) {
+            throw new NullPointerException("Cannot add null");
+        }
+
+        if (getLoadFactor() > 1) {
+            increaseCapacity();
+        }
+
+        Entry<K, V> entry = new AbstractMap.SimpleEntry<>(key, value);
+
+        int index = getIndex(key, entries.length);
+        if (entries[index] == null) {
+            entries[index] = new TiraLinkedList<>();
+        }
+
+        V previousValue = null;
+
+        for (int i = 0; i < entries[index].size(); i++) {
+            Entry<K, V> e = entries[index].get(i);
+            if (e.getKey().equals(key)) {
+                previousValue = e.getValue();
+                entries[index].remove(i);
+                break;
+            }
+        }
+
+        entries[index].add(entry);
+        if (previousValue == null) {
+            //If previous value is null, an item was added to the map
+            itemCount++;
+        }
+
+        return previousValue;
+    }
+
+    private int getIndex(Object o, int arrayLength) {
+        return Math.abs(o.hashCode() % arrayLength);
+    }
+
+    private double getLoadFactor() {
+        return (double)itemCount / (double)entries.length;
+    }
+
+    private class EntrySet extends AbstractSet<Entry<K, V>> {
+        @Override
+        public Iterator<Entry<K, V>> iterator() {
+            return new Iterator<Entry<K, V>>() {
+                private int listIndex = 0;
+                private int valueIndex = 0;
+
+                private int elementListIndex = -1;
+                private int elementValueIndex = -1;
+                private Entry<K, V> next = findNext();
+
+                @Override
+                public boolean hasNext() {
+                    return next != null;
+                }
+
+                @Override
+                public Entry<K, V> next() {
+                    Entry<K, V> value = next;
+                    next = findNext();
+                    return value;
+                }
+
+                private Entry<K, V> findNext() {
+                    Entry<K, V> value = null;
+                    while (value == null) {
+                        if (listIndex == entries.length) {
+                            return null;
+                        }
+
+                        List<Entry<K, V>> list = entries[listIndex];
+                        value = list == null ? null : list.get(valueIndex);
+
+                        elementListIndex = listIndex;
+                        elementValueIndex = valueIndex;
+
+                        valueIndex++;
+                        if (list == null || valueIndex == list.size()) {
+                            valueIndex = 0;
+                            listIndex++;
+                        }
+                    }
+
+                    return value;
+                }
+
+                @Override
+                public void remove() {
+                    if (elementListIndex == -1 || elementValueIndex == -1) {
+                        throw new IllegalStateException();
+                    }
+
+                    entries[elementListIndex].remove(elementValueIndex);
+                    itemCount--;
+                }
+            };
+        }
+
+        @Override
+        public boolean contains(Object o) {
+            if (o instanceof Entry) {
+                V value = TiraHashMap.this.get(((Entry) o).getKey());
+                return value != null && value.equals(((Entry) o).getValue());
+            } else {
+                return false;
+            }
+        }
+
+        @Override
+        public boolean remove(Object o) {
+            if (o instanceof Entry) {
+                return TiraHashMap.this.remove(((Entry) o).getKey()) != null;
+            } else {
+                return false;
+            }
+        }
+
+        @Override
+        public int size() {
+            return itemCount;
+        }
+
+        @Override
+        public void clear() {
+            TiraHashMap.this.clear();
+        }
+
+        @Override
+        public boolean add(Entry<K, V> entry) {
+            int before = TiraHashMap.this.size();
+            TiraHashMap.this.put(entry.getKey(), entry.getValue());
+            return TiraHashMap.this.size() != before;
+        }
+    }
+}
---
+++ src/test/java/xyz/malkki/gtfsroutefinder/datastructures/TiraHashMapTest.java
@@ -0,0 +1,183 @@
+package xyz.malkki.gtfsroutefinder.datastructures;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+public class TiraHashMapTest {
+    private TiraHashMap<String, String> map;
+
+    @Before
+    public void setup() {
+        map = new TiraHashMap<>();
+    }
+
+    @Test
+    public void testPutDifferentKey() {
+        map.put("test", "value");
+        map.put("test_key", "test_value");
+
+        assertEquals(2, map.size());
+    }
+
+    @Test
+    public void testPutSameKey() {
+        map.put("test", "value");
+
+        assertEquals("value", map.get("test"));
+
+        String replaced = map.put("test", "new");
+        assertEquals("value", replaced);
+        assertEquals("new", map.get("test"));
+        assertEquals(1, map.size());
+    }
+
+    @Test
+    public void testPutMany() {
+        for (int i = 0; i < 1000000; i++) {
+            map.put(String.valueOf(i), "test");
+        }
+
+        assertEquals(1000000, map.size());
+    }
+
+    @Test
+    public void testGetNonexistent() {
+        assertNull(map.get("test"));
+    }
+
+    @Test
+    public void testRemoveNonexistent() {
+        map.put("new", "value");
+
+        assertNull(map.remove("test"));
+        assertEquals(1, map.size());
+    }
+
+    @Test
+    public void testContainsKey() {
+        map.put("test", "value");
+
+        assertTrue(map.containsKey("test"));
+    }
+
+    @Test
+    public void testDoesNotContainNonexistentKey() {
+        map.put("test", "value");
+
+        assertFalse(map.containsKey("key"));
+    }
+
+    @Test
+    public void testContainsValue() {
+        map.put("test", "value");
+
+        assertTrue(map.containsValue("value"));
+    }
+
+    @Test
+    public void testDoesNotContainNonexistentValue() {
+        map.put("test", "test");
+
+        assertFalse(map.containsValue("value"));
+    }
+
+    @Test
+    public void testClear() {
+        map.put("test", "value");
+        map.put("key", "value");
+
+        assertEquals(2, map.size());
+
+        map.clear();
+
+        assertEquals(0, map.size());
+    }
+
+    @Test
+    public void testEntrySetClear() {
+        map.put("test", "value");
+        map.put("key", "value");
+
+        assertEquals(2, map.size());
+        assertEquals(2, map.entrySet().size());
+
+        map.entrySet().clear();
+
+        assertEquals(0, map.size());
+        assertEquals(0, map.entrySet().size());
+    }
+
+    @Test
+    public void testEntrySetContains() {
+        map.put("test", "value");
+
+        assertTrue(map.entrySet().contains(new AbstractMap.SimpleEntry<>("test", "value")));
+    }
+
+    @Test
+    public void testEntrySetDoesNotContainNonexistent() {
+        assertFalse(map.entrySet().contains(new AbstractMap.SimpleEntry<>("test", "value")));
+    }
+
+    @Test
+    public void testEntrySetAdd() {
+        assertFalse(map.containsKey("test"));
+
+        map.entrySet().add(new AbstractMap.SimpleEntry<>("test", "value"));
+
+        assertTrue(map.containsKey("test"));
+        assertEquals("value", map.get("test"));
+        assertEquals(1, map.size());
+    }
+
+    @Test
+    public void testEntrySetRemove() {
+        map.put("test", "value");
+        assertEquals("value", map.get("test"));
+
+        map.entrySet().remove(new AbstractMap.SimpleEntry<>("test", "value"));
+
+        assertFalse(map.containsKey("test"));
+        assertEquals(0, map.size());
+    }
+
+    @Test
+    public void testEntrySetIterator() {
+        map.put("test", "value");
+
+        List<String> keys = new ArrayList<>();
+        List<String> values = new ArrayList<>();
+
+        map.entrySet().iterator().forEachRemaining(e -> {
+            keys.add(e.getKey());
+            values.add(e.getValue());
+        });
+
+        assertEquals(1, keys.size());
+        assertEquals(1, values.size());
+        assertTrue(keys.contains("test"));
+        assertTrue(values.contains("value"));
+    }
+
+    @Test
+    public void testEntrySetIteratorRemove() {
+        map.put("test", "value");
+        map.put("key", "test_value");
+
+        Iterator<Map.Entry<String, String>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<String, String> entry = iterator.next();
+
+            if (entry.getKey().equals("key")) {
+                iterator.remove();
+            }
+        }
+
+        assertEquals(1, map.size());
+        assertFalse(map.containsKey("key"));
+    }
+}
