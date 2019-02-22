1aae8d2f048efe4c001f6873fffac68649b730cf
Jaakko Malkki <jaakko.malkki@helsinki.fi>
2019-02-22 18:16:17 +0200

    Don't crash if stops.txt does not contain all stops

--- src/main/java/xyz/malkki/gtfsroutefinder/gtfs/graph/GTFSGraph.java
+++ src/main/java/xyz/malkki/gtfsroutefinder/gtfs/graph/GTFSGraph.java
@@ -193,6 +193,10 @@ public class GTFSGraph extends Graph<Stop> {
 
         for (StopTime stopTime : stopTimesOfTrip) {
             Stop destinationStop = stops.get(stopTime.getStopId());
+            if (destinationStop == null) {
+                //This should happen only if the GTFS feed is malformed
+                return Collections.emptyList();
+            }
 
             long arrivalTimeMillis = calculateTime(departureDate, stopTime.getArrivalTime());
 
