5e7ed7e0de68bd3210b0960ae83570bdb6dc480d
Jaakko Malkki <jaakko.malkki@helsinki.fi>
2019-03-07 20:30:11 +0200

    Fixes + add mode for running time complexity tests

--- src/main/java/xyz/malkki/gtfsroutefinder/Main.java
+++ src/main/java/xyz/malkki/gtfsroutefinder/Main.java
@@ -1,5 +1,7 @@
 package xyz.malkki.gtfsroutefinder;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 import xyz.malkki.gtfsroutefinder.datastructures.TiraArrayList;
 import xyz.malkki.gtfsroutefinder.datastructures.TiraHashMap;
 import xyz.malkki.gtfsroutefinder.graph.Edge;
@@ -11,19 +13,30 @@ import xyz.malkki.gtfsroutefinder.gtfs.graph.StopEdge;
 import xyz.malkki.gtfsroutefinder.gtfs.model.core.Stop;
 import xyz.malkki.gtfsroutefinder.gtfs.utils.GTFSGraphBuilder;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        if (args.length >= 3 && "--time-complexity-test".equals(args[0])) {
+            String outputFile = args[1];
+            String[] feeds = Arrays.copyOfRange(args,2, args.length);
+
+            TimeComplexityTest test = new TimeComplexityTest(feeds);
+            test.runTests(outputFile);
+
+            System.out.println("Time complexity test done, results written to "+outputFile);
+
+            return;
+        }
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Location for GTFS feed:");
         String path = scanner.nextLine();
@@ -38,7 +51,6 @@ public class Main {
         System.out.println("Graph built in "+((System.nanoTime() - start) / 1_000_000)+"ms");
 
         AStar<Stop> aStarRouteFinder = new AStar<>((a,b) -> 1000 * Math.round(a.getLocation().distanceTo(b.getLocation()) / (30 * 3.6)));
-        Dijkstra<Stop> dijkstraRouteFinder = new Dijkstra<>();
 
         loop: while (true) {
             System.out.println("Select action\n\t1 - algorithm comparison\n\t2 - route finder\n\t3 - quit");
@@ -47,7 +59,7 @@ public class Main {
 
                 switch (action) {
                     case 1:
-                        Comparison comparison = new Comparison();
+                        AlgorithmComparison comparison = new AlgorithmComparison();
                         comparison.addAlgorithm("AStar", new AStar<>((a,b) -> 1000 * Math.round(a.getLocation().distanceTo(b.getLocation()) / (30 * 3.6))));
                         comparison.addAlgorithm("AStar (pessimistic, unoptimal routes)", new AStar<>((a,b) -> 1000 * Math.round(a.getLocation().distanceTo(b.getLocation()) / (10 * 3.6))));
                         comparison.addAlgorithm("Dijkstra", new Dijkstra<>());
@@ -55,7 +67,7 @@ public class Main {
                         System.out.println("Number of iterations?");
                         int count = Integer.parseInt(scanner.nextLine());
 
-                        comparison.runComparison(scanner, gtfsGraph, count);
+                        comparison.runComparison(gtfsGraph, count);
                         break;
                     case 2:
                         routeFinder(gtfsGraph, scanner, aStarRouteFinder);
@@ -68,14 +80,69 @@ public class Main {
 
     }
 
-    private static class Comparison {
+    private static class TimeComplexityTest {
+        private int iterations = 10;
+        private Map<String, PathFindingAlgorithm<Stop>> algorithms = new TiraHashMap<>();
+        private String[] feeds;
+
+        public TimeComplexityTest(String... feeds) {
+            this.feeds = feeds;
+
+            algorithms.put("AStar", new AStar<>((a,b) -> 1000 * Math.round(a.getLocation().distanceTo(b.getLocation()) / (30 * 3.6))));
+            algorithms.put("AStar (pessimistic, unoptimal routes)", new AStar<>((a,b) -> 1000 * Math.round(a.getLocation().distanceTo(b.getLocation()) / (10 * 3.6))));
+            algorithms.put("Dijkstra", new Dijkstra<>());
+        }
+
+        public void runTests(String outputFile) {
+            try(CSVPrinter csvPrinter = new CSVPrinter(new BufferedWriter(new FileWriter(outputFile)), CSVFormat.RFC4180)) {
+                csvPrinter.printRecord("Feed", "Algorithm", "Time", "NumberOfStopsInFeed");
+
+                for (String feed : feeds) {
+                    GTFSGraph gtfsGraph = GTFSGraphBuilder.buildFromFiles(feed);
+
+                    List<Stop> origins = Stream.generate(gtfsGraph::getRandomStop).limit(iterations).collect(Collectors.toCollection(() -> new TiraArrayList<>()));
+                    List<Stop> destinations = Stream.generate(gtfsGraph::getRandomStop).limit(iterations).collect(Collectors.toCollection(() -> new TiraArrayList<>()));
+
+                    long time = System.currentTimeMillis();
+
+                    Map<String, Long> timePerAlgorithm = new TiraHashMap<>();
+
+                    for (int i = 0; i < iterations; i++) {
+                        for (Map.Entry<String, PathFindingAlgorithm<Stop>> entry : algorithms.entrySet()) {
+                            String name = entry.getKey();
+                            PathFindingAlgorithm<Stop> algorithm = entry.getValue();
+
+                            long start = System.nanoTime();
+                            algorithm.findPath(gtfsGraph, origins.get(i), time, destinations.get(i));
+                            long end = System.nanoTime();
+
+                            timePerAlgorithm.compute(name, (key, prev) -> (prev == null ? 0 : prev) + (end - start));
+                        }
+
+                        System.out.println("Feed "+feed+", iteration "+(i+1)+"/"+iterations);
+                    }
+
+                    for (Map.Entry<String, Long> entry : timePerAlgorithm.entrySet()) {
+                        String algorithmName = entry.getKey();
+                        long timeInSeconds = TimeUnit.NANOSECONDS.toSeconds(entry.getValue());
+
+                        csvPrinter.printRecord(feed, algorithmName, timeInSeconds, gtfsGraph.getStopCount());
+                    }
+                }
+            } catch (IOException e) {
+                System.err.println("Failed to run time complexity tests: "+e.getMessage());
+            }
+        }
+    }
+
+    private static class AlgorithmComparison {
         private Map<String, PathFindingAlgorithm<Stop>> algorithms = new TiraHashMap<>();
 
         public void addAlgorithm(String name, PathFindingAlgorithm<Stop> algorithm) {
             algorithms.put(name, algorithm);
         }
 
-        public void runComparison(Scanner scanner, GTFSGraph gtfsGraph, int iterations) {
+        public void runComparison(GTFSGraph gtfsGraph, int iterations) {
             List<Stop> origins = Stream.generate(gtfsGraph::getRandomStop).limit(iterations).collect(Collectors.toCollection(() -> new TiraArrayList<>()));
             List<Stop> destinations = Stream.generate(gtfsGraph::getRandomStop).limit(iterations).collect(Collectors.toCollection(() -> new TiraArrayList<>()));
 
@@ -90,8 +157,12 @@ public class Main {
                     PathFindingAlgorithm<Stop> algorithm = entry.getValue();
 
                     long start = System.nanoTime();
-                    algorithm.findPath(gtfsGraph, origins.get(i), time, destinations.get(i));
+                    boolean found = algorithm.findPath(gtfsGraph, origins.get(i), time, destinations.get(i)) != null;
                     long end = System.nanoTime();
+
+                    if (!found) {
+                        System.err.println("Warning! No route found from "+origins.get(i).getName()+" to "+destinations.get(i).getName());
+                    }
 
                     timePerAlgorithm.compute(name, (key, prev) -> (prev == null ? 0 : prev) + (end - start));
                 }
